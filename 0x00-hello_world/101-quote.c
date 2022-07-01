e5e2911472a6e3c58598d7a256e1cb5b06922133
Monica <ushiemonica76@gmail.com>
2022-06-30 21:58:47 -0700

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
- *  * main - Entry point
- *   * Return: Always 1 (Success)
- *    */
+ * main - is displaying Dora Korpar phrase
+ * Return: 1 when program is successfull
+ */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-						\" - Dora Korpar, 2015-10-19\n", 59);
-		return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
