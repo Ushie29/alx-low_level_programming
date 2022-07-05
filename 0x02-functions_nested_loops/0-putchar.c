89069db9033077170d29d72179e51130e10c57c4
Monica <ushiemonica76@gmail.com>
2022-07-05 13:24:27 -0700

    my first commit on function, nested and loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * main - execution start here
+  * a program that print _putchar
+  * Return: 0 always successful
+  */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+the work or directory will be working with the directory name 0x02-functions_nested_loops
