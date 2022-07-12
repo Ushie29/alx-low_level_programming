6b383c1be2a83ab4c107e8011a5765632727592c
Monica <ushiemonica76@gmail.com>
2022-07-12 16:33:41 -0700

    task 5 commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - reverses the position of char in string
+ * @s: string pointer
+ * Returns: void.
+ */
+void rev_string(char *s)
+{
+	int i = 0, j = 0;
+	char temp[500]
+
+	while (*(s + i))
+	{
+		*(temp + i) = *(s + i);
+		i++;
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+		*(s + i) = *(temp + j);
+		j++;
+		i--;
+	}
+}
