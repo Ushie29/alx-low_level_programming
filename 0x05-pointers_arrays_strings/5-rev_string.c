8c96ff5ab8903da89b4e7b8dc50bbe5f489eb303
Monica <ushiemonica76@gmail.com>
2022-07-12 16:37:49 -0700

    task 5 commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,12 +7,12 @@
 void rev_string(char *s)
 {
 	int i = 0, j = 0;
-	char temp[500]
+	char temp[500];
 
 	while (*(s + i))
 	{
 		*(temp + i) = *(s + i);
-		i++;
+	i++;
 	}
 	i = i - 1;
 	while (i >= 0)
