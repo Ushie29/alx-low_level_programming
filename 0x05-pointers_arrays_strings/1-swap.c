6aceb8eadc19518435ce23a0041497be55b83471
Monica <ushiemonica76@gmail.com>
2022-07-12 14:15:41 -0700

    task 1 commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-  * swap_int - swaps the values of two integers
-  * @a: integer 1
-  * @b: integer 2
-  *Return: void.
-  */
-void swap_int(int *a, int *b);
+ * swap_int - swaps the values of two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: void.
+ */
+void swap_int(int *a, int *b)
 {
 	int tmp = *a;
 
