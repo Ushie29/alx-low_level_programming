1049254a76871f35a6bd17949663aa46547eb866
Monica <ushiemonica76@gmail.com>
2022-07-22 13:16:40 -0700

    correction on task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,30 +1,15 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
 /**
- * create_array - creates an array of chars, and
- * initializes it with a character c.
- * @size: size of array
- * @c: array initializer
+ * main - prints program name, followed by a new line
+ * @argc: argument counter
+ * @argv: argument vector
  *
- * Return: pointer to array or NULL if size is 0
+ * Return: 0
  */
-char *create_array(unsigned int size, char c)
+
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	char *array = NULL;
-	unsigned int i;
-
-	/* set array size*/
-	if (size != 0)
-	{
-		array = (char *) malloc(size * sizeof(char));
-		/* fill array with c */
-		if (array != NULL)
-		{
-			for (i = 0; i < size; i++)
-				array[i] = c;
-		}
-	}
-
-	return (array);
+	printf("%s\n", argv[0]);
+	/* or used (void)argc; */
+	return (0);
 }
