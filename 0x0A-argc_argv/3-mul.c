c457727fdb5ef0b356d90846ba13d5f7b3d2dce7
Monica <ushiemonica76@gmail.com>
2022-07-22 15:04:55 -0700

    task 3 correction

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main - adds all valid numbers passed to main, or Error
+ * main - multiply 2 numbers passed to main, or Error
  * @argc: argument count
  * @argv: argument vector of strings
  * Return: 1 if error, 0 if function runs correctly
  */
+
 int main(int argc, char *argv[])
 {
-	int total = 0, i, arg;
-	char *ptr;
-
-	if (argc > 1)
+	if (argc == 3)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			/* strol converts strings to long int */
-			arg = strtol(argv[i], &ptr, 10);
-
-			if (!*ptr)
-				total += arg;
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
 	}
-	printf("%d\n", total);
-	return (0);
+	else
+		printf("Error\n");
+
+	return (1);
 }
